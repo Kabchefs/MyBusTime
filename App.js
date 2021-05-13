d8bdfea1fc90e04952c0f862f26b8e406b4d014f
Pawan Singhla <pawansinghla300@gmail.com>
2021-05-13 11:35:23 +0530

    Register Page & stackNavigation

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 
 import React from 'react';
-
 import Login from './components/Login';
-import { Provider as PaperProvider,DefaultTheme } from 'react-native-paper';
+import Register from './components/Register';
+import OTPVerify from './components/OTPVerify';
+import { NavigationContainer,useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
+const Stack = createStackNavigator();
 export default function App() {
+ 
   return (
-    
-    <PaperProvider theme={theme}>
-   <Login/>
-   </PaperProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" >
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="OTP Verfiy" component={OTPVerify} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: 'white',
-  },
-};
