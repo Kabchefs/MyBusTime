f54b118174865fc796df23e1c954e789c05dbde6
Pawan Singhla <pawansinghla300@gmail.com>
2021-06-19 22:49:15 +0530

    Side Drawer

--- navigation/MbtNavigator.js
+++ navigation/MbtNavigator.js
@@ -17,36 +17,47 @@ import ConnectScreen from '../screens/ConnectScreen';
 import StopDetailsScreen from '../screens/StopDetailsScreen';
 import RouteDetailsScreen from '../screens/RouteDetailsScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator} from 'react-navigation-drawer';
 import PrivacyPolicyScreen from '../screens/PrivacyPolicyScreen';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Dimensions } from 'react-native';
+import ContentComponent from './ContentComponent';
+import LeaderBoardScreen from '../screens/LeaderBoardScreen';
 
-const HomePage= createMaterialBottomTabNavigator({
-    Main:{screen:HomeRoute,navigationOptions:{
-        tabBarLabel:'Home',
-        
-        tabBarIcon:(tabOp)=>{
-          
-            return <Ionicons name="home"  size={25}  color="white"/>;
-        },
-        tabBarColor:'rgb(23, 157, 227)'
-    }},
-    Connect:{screen:ConnectScreen,navigationOptions:{
-        tabBarLabel:'Connect',
-        
-        tabBarIcon:(tabOp)=>{
-            return <Ionicons name="search" size={25} color="white" />;
-        },
-        tabBarColor:'rgb(23, 157, 227)'
-    }},
-    Profile:{screen:ProfileScreen,navigationOptions:{
-         tabBarLabel:'Profile',
-        tabBarIcon:(tabOp)=>{
+
+const windowWidth = Dimensions.get('window').width;
+
+const HomePage = createMaterialBottomTabNavigator({
+  Main: {
+    screen: HomeRoute, navigationOptions: {
+      tabBarLabel: 'Home',
+
+      tabBarIcon: (tabOp) => {
+
+        return <Ionicons name="home" size={25} color="white" />;
+      },
+      tabBarColor: 'rgb(23, 157, 227)'
+    }
+  },
+  Connect: {
+    screen: ConnectScreen, navigationOptions: {
+      tabBarLabel: 'Connect',
+
+      tabBarIcon: (tabOp) => {
+        return <Ionicons name="search" size={25} color="white" />;
+      },
+      tabBarColor: 'rgb(23, 157, 227)'
+    }
+  },
+  Profile: {
+    screen: ProfileScreen, navigationOptions: {
+      tabBarLabel:'Profile',
+      tabBarIcon:(tabOp)=>{
             return <Ionicons name="person" size={25} color="white" />;
         },
-        tabBarColor:'rgb(23, 157, 227)'
-    }}
-   
+       tabBarColor:'rgb(23, 157, 227)'
+    }
+}
 },{
     activeColor:'white',
     shifting:true
@@ -66,6 +77,7 @@ const MbtNavigator=createStackNavigator({
     RouteDetails:RouteDetailsScreen,    
     PrivacyPolicy:PrivacyPolicyScreen,
     Connect:ConnectScreen,
+    LeaderBoard:LeaderBoardScreen
 })
 
 // const Logoff=()=>{
@@ -75,100 +87,38 @@ const MbtNavigator=createStackNavigator({
 //     )
 // }
 
-const MainNavigator=createDrawerNavigator({
-    Home: {
-        screen:MbtNavigator,
-        navigationOptions :{
-            itemStyle:{marginTop:30},
-            drawerLabel: 'Home',
-            drawerIcon: ({ tintColor }) => (
-              <MaterialCommunityIcons name="home" size={24}  color='rgb(23, 157, 227)' style={{marginRight:-10}}/>
-            ),
-          },
-          contentOptions: {
-            activeTintColor: '#e91e63',
-            itemsContainerStyle: {
-              marginTop:50,
-            },
-            iconContainerStyle: {
-              opacity: 1
-            },
-            itemStyle:{
-                marginTop:100,
-                paddingTop:30,
-            }
-          },
-         
-        
-      },
-      Profile: {
-        screen: ProfileScreen,
-        navigationOptions: {
-    
-            drawerLabel: 'Profile',
-            drawerIcon: ({ tintColor }) => (
-              <MaterialCommunityIcons name="account" size={22} color="rgb(23, 157, 227)"  style={{marginRight:-10}} />
-            ),
-          }
-        
-      },
-      Connect:{
-        screen: ConnectScreen,
-        navigationOptions: {
-    
-            drawerLabel: 'Connect',
-            drawerIcon: ({ tintColor }) => (
-              <Ionicons name="search" size={22} color="rgb(23, 157, 227)" style={{marginRight:-10}} />
-            ),
-          }
-        
+const MainNavigator = createDrawerNavigator(  {
+  Home: {
+    screen:MbtNavigator,
+  },
+  Profile: {
+    screen: ProfileScreen
+  },
+  Connect: {
+    screen: ConnectScreen,
+  },
 
+  PrivacyPolicy: {
+    screen: PrivacyPolicyScreen
+  },
 
-      },
+}, {
+  initialRouteName: 'Home',
+  drawerBackgroundColor: '#ffffff',
+  drawerLockMode: 'locked-open',
+  drawerType: 'front',
+  drawerWidth: windowWidth * 3 / 4,
+  edgeWidth: 100,
+  contentComponent:ContentComponent,
+  contentOptions: {
+    activeTintColor: "rgb(23, 157, 227)",
+    inactiveTintColor: '#000000',
+    itemStyle: {
+      marginTop: 15,
+    }
 
-      PrivacyPolicy: {
-        screen: PrivacyPolicyScreen,
-        navigationOptions: {
-    
-            drawerLabel: 'Privacy Policy',
-            drawerIcon: ({ tintColor }) => (
-              <MaterialCommunityIcons name="information" size={24} color="rgb(23, 157, 227)"  style={{marginRight:-10}} />
-            ),
-          }
-      },
-      ResetPassword: {
-        screen:ResetPasswordScreen,
-        navigationOptions: {
-    
-            drawerLabel: 'Reset Password',
-            drawerIcon: ({ tintColor }) => (
-              <MaterialCommunityIcons name="security" size={22} color="rgb(23, 157, 227)"  style={{marginRight:-10}}/>
-            ),
-          }
-       
-      },
-   
-    })
+  }
 
-// const MainNavigtor=createDrawerNavigator(
-//     {
-       
-
-//     //   Main:MbtNavigator,
-//     //   PrivacyPolicy:PrivacyPolicyScreen,
-//     //   ResetPassword:ResetPasswordScreen,
-//     // //   }
-//     // //   ,{
-//     // //   contentOptions: {
-//     // //      // paddingTop:80,
-//     // //     activeTintColor: 'rgb(23, 157, 227)',
-//     // //     itemStyle:{
-//     // //         marginTop:20
-//     // //     }
-//     // //   },
-
-        
-//     });
-
+})
 
 export default createAppContainer(MainNavigator);
