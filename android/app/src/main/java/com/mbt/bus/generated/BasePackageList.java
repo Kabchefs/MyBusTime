8dfd6c156fd71290518fb0e6f12d152c9f4d1020
Pawan Singhla <pawansinghla300@gmail.com>
2021-09-29 08:56:35 +0530

    'eject-app'

---
+++ android/app/src/main/java/com/mbt/bus/generated/BasePackageList.java
@@ -0,0 +1,28 @@
+package com.mbt.bus.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.appauth.AppAuthPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.application.ApplicationPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.contacts.ContactsPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.facebook.FacebookPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.notifications.NotificationsPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.updates.UpdatesPackage()
+    );
+  }
+}
